300e0f21bb6dd698feb6a7161b4646c0d756fad6
Abubakar Abrar <abubakarabrar@gmail.com>
2026-02-04 14:32:59 +0300

    Update README with latest changes

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,6 +7,7 @@ resource "aws_vpc" "main" {
   }
 }
 resource "aws_subnet" "public" {
+    count  = 2
     vpc_id = aws_vpc.main.id
     cidr_block = var.public_subnet_cidrs[count.index]
     availability_zone = var.availability_zones[count.index]
@@ -16,10 +17,11 @@ resource "aws_subnet" "public" {
   }
 }
 resource "aws_subnet" "private" {
+    count  = 2
     vpc_id = aws_vpc.main.id
     cidr_block = var.private_subnet_cidrs[count.index]
     availability_zone = var.availability_zones[count.index]
-    map_public_ip_on_launch = true
+    map_public_ip_on_launch = false
     tags = {
       Name = "private-subnet-${count.index + 1}"
   }
